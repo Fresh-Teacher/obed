dd95da46082b955013910c01cd533f5073ab6470
Fresh-Teacher <98003374+Fresh-Teacher@users.noreply.github.com>
2024-03-19 10:26:27 +0200

    add missing links

---
+++ signin.php
@@ -0,0 +1,15 @@
+	<html>
+		<head>
+			<title>
+			</title>
+		</head>
+		<body style='font-family: "Myriad Set Pro","Lucida Grande","Helvetica Neue","Helvetica","Arial","Verdana","sans-serif"; font-size:13px;'>
+			<form style="width: 30%; border: 1px solid #E0E0E0; padding: 10px; margin: auto; " name="login" action="signin.php?next=1" method="post">
+				<div><u>Admin Login</u></div>	<br/>			
+				<div style="float: left; width:30%;">Username:</div> <input style="float: left;" name="username" type="text" /><div style="clear: both;margin-top: 10px;"></div>
+				<div style="float: left; width:30%;">Password:</div> <input style="float: left;" name="password" type="password" /><div style="clear: both;"></div>
+				<input style="float: right;" name="login" type="submit" value="Login" /><div style="clear: both;"></div>
+			</form>
+			<div style="margin: 5px; text-align: center;"><a href="index.php">Go to the website</a></div>
+		</body>
+	</html>
